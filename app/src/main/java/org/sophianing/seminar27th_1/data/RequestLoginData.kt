7277b0d8e03c12b4cea4ca73f66a4e99cad49690
sohpianing <phj8221@naver.com>
2020-11-29 12:28:23 +0900

    [ADD] RequestLoginData, ResponseLoginData

---
+++ app/src/main/java/org/sophianing/seminar27th_1/data/RequestLoginData.kt
@@ -0,0 +1,6 @@
+package org.sophianing.seminar27th_1.data
+
+data class RequestLoginData(
+    val email : String,
+    val password : String
+)
---
+++ app/src/main/java/org/sophianing/seminar27th_1/data/ResponseLoginData.kt
@@ -0,0 +1,14 @@
+package org.sophianing.seminar27th_1.data
+
+data class ResponseLoginData(
+    val data: Data,
+    val message: String,
+    val status: Int,
+    val success: Boolean
+) {
+    data class Data(
+        val email: String,
+        val password: String,
+        val userName: String
+    )
+}
